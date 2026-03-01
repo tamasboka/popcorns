a4219c0123d2dfcfacbd774c287de11f2d379bab
Bóka Tamás <tomi500offical@gmail.com>
2026-03-01 02:06:23 +0100

    home, login, register routing

--- basic-frontend/src/components/layouts/NavBar.vue
+++ basic-frontend/src/components/layouts/NavBar.vue
@@ -1,9 +1,32 @@
 <script>
 
+export default {
+  name: "NavBar",
+  computed: {
+    Register() {
+      return Register
+    }
+  }
+}
 </script>
-<template>
 
+<template>
+  <nav class="navbar expand-lg bg-dark">
+    <div class="container">
+      <RouterLink :to="{name: 'home'}" class="navbar-brand text-white h2">
+        Home
+      </RouterLink>
+      <div v-if="true">
+        <RouterLink :to="{name: 'register'}" class="btn btn-success mx-2">Register</RouterLink>
+        <RouterLink :to="{name: 'login'}" class="btn btn-success mx-2">Login</RouterLink>
+      </div>
+      <div v-else>
+
+      </div>
+    </div>
+  </nav>
 </template>
+
 <style scoped>
 
 </style>
