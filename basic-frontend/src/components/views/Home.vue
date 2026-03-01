4a776054ff591449bd1e55bb0d59183549610adc
Bóka Tamás <tomi500offical@gmail.com>
2026-03-01 02:05:09 +0100

    get request to api/movies sent and stored

--- basic-frontend/src/components/views/Home.vue
+++ basic-frontend/src/components/views/Home.vue
@@ -1,6 +1,21 @@
 <script>
+import axios from "axios"
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      movies: null
+    }
+  },
+  async mounted() {
+    await axios({
+      method: "get",
+      url: "http://127.0.0.1:8000/api/movies",
+      headers: {
+        accept: "application/json"
+      }
+    }).then(res => this.movies = res.data.data)
+  }
 }
 </script>
 
