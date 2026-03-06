8dd2272fc9fc963a5d6cb65a4504942316f09d3e
Bóka Tamás <tomi500offical@gmail.com>
2026-03-06 21:16:40 +0100

    add basic data for seeding of Users, GenreMovies and Ratings

--- basic-backend/database/seeders/GenreMovieSeeder.php
+++ basic-backend/database/seeders/GenreMovieSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\GenreMovie;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,25 @@ class GenreMovieSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        GenreMovie::create([
+            'genre_id' => 1,
+            'movie_id' => 1
+        ]);
+        GenreMovie::create([
+            'genre_id' => 2,
+            'movie_id' => 1
+        ]);
+        GenreMovie::create([
+            'genre_id' => 3,
+            'movie_id' => 1
+        ]);
+        GenreMovie::create([
+            'genre_id' => 1,
+            'movie_id' => 2
+        ]);
+        GenreMovie::create([
+            'genre_id' => 5,
+            'movie_id' => 2
+        ]);
     }
 }
--- basic-backend/database/seeders/RatingSeeder.php
+++ basic-backend/database/seeders/RatingSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Rating;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,21 @@ class RatingSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Rating::create([
+            'movie_id' => 1,
+            'rated_by' => 1,
+            'rating' => 9.5,
+            'rating_title' => 'jó film',
+            'rating_comment' => 'tetszett',
+            'has_spoilers' => 0
+        ]);
+        Rating::create([
+            'movie_id' => 1,
+            'rated_by' => 2,
+            'rating' => 1.5,
+            'rating_title' => 'nem jó film',
+            'rating_comment' => 'nem tetszett',
+            'has_spoilers' => 0
+        ]);
     }
 }
