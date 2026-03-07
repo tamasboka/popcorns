b26124ada7cf354ee76e2ae3b79f93ece5de9486
Bóka Tamás <tomi500offical@gmail.com>
2026-03-07 19:18:24 +0100

    add seeding data for watchlists

--- basic-backend/database/seeders/WatchlistSeeder.php
+++ basic-backend/database/seeders/WatchlistSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Watchlist;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class WatchlistSeeder extends Seeder
 {
@@ -12,6 +13,37 @@ class WatchlistSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Watchlist::create([
+            'user_id' => 1,
+            'name' => 'Star Wars original trilogy'
+        ]);
+        Watchlist::create([
+            'user_id' => 2,
+            'name' => 'Star Wars prequel trilogy'
+        ]);
+        DB::table('watchlist_movies')->insert([
+            'watchlist_id' => 1,
+            'movie_id' => 1,
+        ]);
+        DB::table('watchlist_movies')->insert([
+            'watchlist_id' => 1,
+            'movie_id' => 2,
+        ]);
+        DB::table('watchlist_movies')->insert([
+            'watchlist_id' => 1,
+            'movie_id' => 3,
+        ]);
+        DB::table('watchlist_movies')->insert([
+            'watchlist_id' => 2,
+            'movie_id' => 4,
+        ]);
+        DB::table('watchlist_movies')->insert([
+            'watchlist_id' => 2,
+            'movie_id' => 5,
+        ]);
+        DB::table('watchlist_movies')->insert([
+            'watchlist_id' => 2,
+            'movie_id' => 6,
+        ]);
     }
 }
