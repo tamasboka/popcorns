cc9d41a41d6a9febbe4631cc7496ef85fc3bfdae
Bóka Tamás <tomi500offical@gmail.com>
2026-03-07 17:30:12 +0100

    Reapply "add /users api with basic index and show methods"
    This reverts commit e480a2ee00dcec81c34f8babad79fae400b0c240.

---
+++ basic-backend/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\User\UserCollection;
+use App\Http\Resources\User\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+        return (new UserCollection($users))
+            ->response()
+            ->setStatusCode(200);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $user = User::findOrFail($id);
+        return (new UserResource($user))
+            ->response()
+            ->setStatusCode(200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
