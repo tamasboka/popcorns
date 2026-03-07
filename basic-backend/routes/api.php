e480a2ee00dcec81c34f8babad79fae400b0c240
Bóka Tamás <tomi500offical@gmail.com>
2026-03-07 17:27:32 +0100

    Revert "add /users api with basic index and show methods"
    This reverts commit 3aba8c28e2ef9a4f5ced7fb62cccbb9ba91f2f55.

--- basic-backend/routes/api.php
+++ basic-backend/routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MovieController;
 
 Route::apiResource('/movies',MovieController::class);
-Route::apiResource('/users', UserController::class);
