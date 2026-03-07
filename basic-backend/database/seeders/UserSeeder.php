adf21ee77323fecacb06f8a50ccb1cc81620cf1a
Bóka Tamás <tomi500offical@gmail.com>
2026-03-07 19:18:57 +0100

    add seeder for watched movies

--- basic-backend/database/seeders/UserSeeder.php
+++ basic-backend/database/seeders/UserSeeder.php
@@ -3,9 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -25,6 +24,15 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'different tamas',
             'password' => 'a'
+        ]);
+
+        DB::table('movies_watched')->insert([
+            'movie_id' => 1,
+            'user_id' => 1,
+        ]);
+        DB::table('movies_watched')->insert([
+            'movie_id' => 6,
+            'user_id' => 1,
         ]);
     }
 }
