626dc3790d3c1355b3c87cc5b58fb624351556e3
Bóka Tamás <tomi500offical@gmail.com>
2026-03-15 23:37:09 +0100

    feat: loading theme from localStorage

--- basic-frontend/src/App.vue
+++ basic-frontend/src/App.vue
@@ -1,5 +1,15 @@
 <script>
-export default {}
+export default {
+  methods: {
+    setTheme() {
+      const savedTheme = localStorage.getItem('theme');
+      document.documentElement.setAttribute('data-bs-theme', savedTheme);
+    }
+  },
+  mounted() {
+    this.setTheme()
+  }
+}
 </script>
 
 <template>
