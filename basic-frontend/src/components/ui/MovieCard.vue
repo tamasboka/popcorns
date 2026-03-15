9277fa4eba4843722a1c5174e46987ff0bad8c34
Bóka Tamás <tomi500offical@gmail.com>
2026-03-15 21:22:00 +0100

    feat: create additional components for error and movie card

---
+++ basic-frontend/src/components/ui/MovieCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: "MovieCard",
+  props: {
+    title: {
+      type: String
+    },
+    avg: {
+      type: Number
+    },
+    year: {
+      type: Number
+    }
+  }
+}
+</script>
+
+<template>
+<div>
+  <p>{{ title }}</p>
+  <p>{{ avg }}⭐</p>
+  <p>{{ year }}</p>
+</div>
+</template>
+
+<style scoped>
+div {
+  border: 5px solid black;
+  width: 25%;
+}
+</style>
---
+++ basic-frontend/src/views/Error/Forbidden.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "Forbidden"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
