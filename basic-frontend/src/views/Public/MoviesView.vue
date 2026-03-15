38ba71808f2872ec99b04e1be61a08fe882913ee
Bóka Tamás <tomi500offical@gmail.com>
2026-03-15 21:23:57 +0100

    feat: add getMovies method that runs automatically, add MovieCard component for each movie

--- basic-frontend/src/views/Public/MoviesView.vue
+++ basic-frontend/src/views/Public/MoviesView.vue
@@ -1,11 +1,34 @@
 <script>
+import {getAllMovies} from "@/data/data.js";
+import MovieCard from "@/components/ui/MovieCard.vue";
+
 export default {
-  name: "MoviesView"
+  name: "MoviesView",
+  components: {MovieCard},
+  data() {
+    return {
+      movies: [],
+      error: false
+    }
+  },
+  methods: {
+    async getMovies() {
+      try {
+        const res = await getAllMovies()
+        this.movies = res.data.data
+      } catch {
+        this.error = true
+      }
+    }
+  },
+  mounted() {
+    this.getMovies()
+  }
 }
 </script>
 
 <template>
-
+<MovieCard v-for="movie in movies" :key="movie.id" :title="movie.title" :avg="movie.avg" :year="movie.release_year"/>
 </template>
 
 <style scoped>
