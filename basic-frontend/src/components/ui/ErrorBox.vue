20158809568337d84bd79f6dee6d60e7446d4a1e
Bóka Tamás <tomi500offical@gmail.com>
2026-03-16 17:54:37 +0100

    refactor: remove .box styles due to it being moved to style.css

--- basic-frontend/src/components/ui/ErrorBox.vue
+++ basic-frontend/src/components/ui/ErrorBox.vue
@@ -14,14 +14,10 @@ export default {
     <h1>{{ getTitle }}</h1>
     <RouterLink :to="{name: 'public-home'}" class="btn btn-success mx-2"><i class="bi bi-house"></i></RouterLink>
     <RouterLink :to="{name: 'public-movies'}" class="btn btn-success mx-2"><i class="bi bi-film"></i></RouterLink>
+    <RouterLink :to="{name: 'public-shows'}" class="btn btn-success mx-2"><i class="bi bi-collection-play"></i></RouterLink>
   </div>
 </template>
 
 <style scoped>
-.box {
-  padding: 50px;
-  color: black;
-  box-shadow: 20px 20px black;
-  background-color: whitesmoke;
-}
+
 </style>
