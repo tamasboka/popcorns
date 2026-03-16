8080ca90d5307120e85613ddcfec76540197d338
Bóka Tamás <tomi500offical@gmail.com>
2026-03-16 18:21:45 +0100

    feat: create boxes for registration and login forms

--- basic-frontend/src/views/Auth/LoginView.vue
+++ basic-frontend/src/views/Auth/LoginView.vue
@@ -1,11 +1,25 @@
 <script>
+import {Form, Field} from 'vee-validate'
 export default {
-  name: "LoginView"
+  name: "LoginView",
+  components: {Field, Form}
 }
 </script>
 
 <template>
-
+<div class="box">
+  <RouterLink :to="{name: 'public-home'}" class="btn btn-primary btn-sm mb-3"><i class="bi bi-arrow-left-circle"></i></RouterLink>
+  <Form>
+    <h1 class="h4 text-center">Login</h1>
+    <label for="name">Name</label>
+    <Field name="name" id="name" class="form-control mb-2"/>
+    <label for="password">Password</label>
+    <Field name="password" id="password" class="form-control mb-2" type="password"/>
+    <div class="d-flex justify-content-center mt-5">
+      <button class="btn btn-success">Login</button>
+    </div>
+  </Form>
+</div>
 </template>
 
 <style scoped>
--- basic-frontend/src/views/Auth/RegisterView.vue
+++ basic-frontend/src/views/Auth/RegisterView.vue
@@ -1,11 +1,30 @@
 <script>
+import {Field, Form} from "vee-validate";
+
 export default {
-  name: "RegisterView"
+  name: "RegisterView",
+  components: {Field, Form}
 }
 </script>
 
 <template>
-
+  <div class="box">
+    <RouterLink :to="{name: 'public-home'}" class="btn btn-primary btn-sm mb-3"><i class="bi bi-arrow-left-circle"></i></RouterLink>
+    <Form>
+      <h1 class="h4 text-center">Register</h1>
+      <label for="name">Name</label>
+      <Field name="name" id="name" class="form-control mb-2"/>
+      <label for="password">Password</label>
+      <Field name="password" id="password" class="form-control mb-2" type="password"/>
+      <label for="password_1">Repeat password</label>
+      <Field name="password_1" id="password_1" class="form-control mb-2" type="password"/>
+      <label for="birthdate">Birthday</label>
+      <Field name="birthdate" id="birthdate" class="form-control mb-2" type="date"/>
+      <div class="d-flex justify-content-center mt-5">
+        <button class="btn btn-success">Register</button>
+      </div>
+    </Form>
+  </div>
 </template>
 
 <style scoped>
