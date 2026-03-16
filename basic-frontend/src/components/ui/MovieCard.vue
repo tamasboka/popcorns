76a39c37b8cd536ef7f4cd09f3c14903c9fa3d86
Bóka Tamás <tomi500offical@gmail.com>
2026-03-16 17:56:14 +0100

    refactor: fix routing, add checking for empty avg rating

--- basic-frontend/src/components/ui/MovieCard.vue
+++ basic-frontend/src/components/ui/MovieCard.vue
@@ -12,9 +12,9 @@ export default {
 
 <template>
   <div class="card m-2">
-    <div class="card-body text-center" @click="$router.push({name: 'public-movie', params: {movieID: id}})">
+    <div class="card-body text-center" @click="$router.push({name: 'public-movie', params: {movieID: movie.id}})">
       <p class="card-text">{{ movie.title }}</p>
-      <p class="card-text">{{ movie.avg }}⭐</p>
+      <p class="card-text">{{ movie.avg? movie.avg+'⭐' : 'No ratings yet' }}</p>
       <p class="card-text">{{ movie.release_year }}</p>
     </div>
   </div>
@@ -23,9 +23,8 @@ export default {
 <style scoped>
 .card {
   border: 5px solid black;
-  transition: transform 0.2s;
+  transition: transform 0.25s;
   background: gray;
-  //background: linear-gradient(10deg, rgb(29, 19, 141) 5%, rgb(54, 136, 153) 75%);
 }
 
 .card:hover {
