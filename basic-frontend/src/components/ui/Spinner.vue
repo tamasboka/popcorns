1b6325ebb3b235eeb3d28a30c9de2501f94cbc08
Bóka Tamás <tomi500offical@gmail.com>
2026-03-16 13:35:03 +0100

    feat: create spinner for async loading

---
+++ basic-frontend/src/components/ui/Spinner.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "Spinner"
+}
+</script>
+
+<template>
+  <div class="d-flex justify-content-center align-items-center min-vh-100">
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
