5d09cad0850225b91642b6b576b2bb17879bd2f9
Bóka Tamás <tomi500offical@gmail.com>
2026-03-23 00:06:33 +0100

    feat: implement ratings on MovieView

---
+++ basic-frontend/src/components/ui/Review.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  name: "Review",
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    stars: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    has_spoilers: {
+      type: Number,
+      required: true
+    },
+    uid: {
+      type: Number,
+      required: true
+    },
+    uname: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+<div>
+  <RouterLink :to="{name: 'user-profile', params: {userID: uid}}" class="btn btn-outline-light">{{ uname }}</RouterLink>
+  <p v-if="has_spoilers === 1">Spoiler alert</p>
+  <h1>{{ title }} - {{ stars }}⭐</h1>
+  <p> {{ content }} </p>
+</div>
+</template>
+
+<style scoped>
+
+</style>
