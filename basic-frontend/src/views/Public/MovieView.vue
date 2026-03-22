3bc1c4da4e9dc9fb34d0bc55ff81280454506d9a
Bóka Tamás <tomi500offical@gmail.com>
2026-03-23 00:06:18 +0100

    feat: implement ratings on MovieView

--- basic-frontend/src/views/Public/MovieView.vue
+++ basic-frontend/src/views/Public/MovieView.vue
@@ -1,6 +1,9 @@
 <script>
+import Review from "@/components/ui/Review.vue";
+
 export default {
   name: "MovieView",
+  components: {Review},
   data() {
     return {
       movie: this.$route.meta.prefetched.movie.data.data
@@ -10,7 +13,17 @@ export default {
 </script>
 
 <template>
+  <section class="row p-5">
+    <div class="col-12 col-lg-8">
 
+    </div>
+    <div class="col-12 col-lg-4">
+      <div v-for="review in movie.ratings" class="box">
+        <Review :id="review.id" :title="review.title" :content="review.content" :has_spoilers="review.has_spoilers"
+                :stars="review.stars" :uid="review.rater.id" :uname="review.rater.name"/>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
