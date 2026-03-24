2e46a07ffb3e54f5b23c4ba706079518d8571f8e
Bóka Tamás <tomi500offical@gmail.com>
2026-03-24 22:30:52 +0100

    feat: make movie details and reviews box, reviews that contain spoilers now can only be seen after clicked

--- basic-frontend/src/components/ui/Review.vue
+++ basic-frontend/src/components/ui/Review.vue
@@ -1,6 +1,11 @@
 <script>
 export default {
   name: "Review",
+  data() {
+    return {
+      revealed: this.has_spoilers === 0
+    }
+  },
   props: {
     id: {
       type: Number,
@@ -36,10 +41,17 @@ export default {
 
 <template>
 <div>
-  <RouterLink :to="{name: 'user-profile', params: {userID: uid}}" class="btn btn-outline-light">{{ uname }}</RouterLink>
-  <p v-if="has_spoilers === 1">Spoiler alert</p>
-  <h1>{{ title }} - {{ stars }}⭐</h1>
-  <p> {{ content }} </p>
+  <RouterLink :to="{name: 'user-profile', params: {userID: uid}}" class="btn btn-sm btn-outline-light">{{ uname }}</RouterLink>
+  <div v-if="revealed">
+    <h1 class="h4">{{ title }} - {{ stars }}⭐</h1>
+    <p> {{ content }} </p>
+  </div>
+  <div v-else>
+    <div class="alert alert-danger text-center">
+      <h1>Spoiler alert!</h1>
+      <button class="btn btn-dark" @click="revealed = true">Click to reveal review</button>
+    </div>
+  </div>
 </div>
 </template>
 
--- basic-frontend/src/views/Public/MovieView.vue
+++ basic-frontend/src/views/Public/MovieView.vue
@@ -1,9 +1,11 @@
 <script>
 import Review from "@/components/ui/Review.vue";
+import ReviewsBox from "@/components/ui/ReviewsBox.vue";
+import MovieDetails from "@/components/ui/MovieDetails.vue";
 
 export default {
   name: "MovieView",
-  components: {Review},
+  components: {MovieDetails, ReviewsBox, Review},
   data() {
     return {
       movie: this.$route.meta.prefetched.movie.data.data
@@ -14,14 +16,11 @@ export default {
 
 <template>
   <section class="row p-5">
-    <div class="col-12 col-lg-8">
-
+    <div class="col-12 col-lg-8 col-md-6">
+      <MovieDetails :movie="movie"/>
     </div>
-    <div class="col-12 col-lg-4">
-      <div v-for="review in movie.ratings" class="box">
-        <Review :id="review.id" :title="review.title" :content="review.content" :has_spoilers="review.has_spoilers"
-                :stars="review.stars" :uid="review.rater.id" :uname="review.rater.name"/>
-      </div>
+    <div class="col-12 col-lg-4 col-md-6">
+      <ReviewsBox :ratings="movie.ratings"/>
     </div>
   </section>
 </template>
