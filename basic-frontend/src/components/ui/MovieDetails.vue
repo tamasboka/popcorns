2a36eefddb6bdcec604d66fa307a36d56d25e2f4
Bóka Tamás <tomi500offical@gmail.com>
2026-03-24 22:31:27 +0100

    feat: make movie details and reviews box, reviews that contain spoilers now can only be seen after clicked

---
+++ basic-frontend/src/components/ui/MovieDetails.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  name: "MovieDetails",
+  props: {
+    movie: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    releaseDate() {
+      let date = this.movie.release_year
+      if (this.movie.release_month && this.movie.release_day) date += ". " + this.movie.release_month + ". " + this.movie.release_day
+      return date
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="box" v-if="movie">
+    <div class="row text-center">
+      <div class="col-12">
+        <h1 class="text-center">{{ movie.title }}</h1>
+      </div>
+      <div class="col-12 col-lg-3 mt-2">
+        <h2>Rating</h2>
+        <h2 v-if="movie.avg">⭐{{ movie.avg }} / 10</h2>
+        <h2 v-else>No ratings</h2>
+      </div>
+      <div class="col-12 col-lg-3 mt-2">
+        <h2>Release date</h2>
+        <h2>{{ releaseDate }}</h2>
+      </div>
+      <div class="col-12 col-lg-3 mt-2">
+        <h2>Runtime</h2>
+        <h2>{{ movie.hours }}h</h2>
+      </div>
+      <div class="col-12 col-lg-3 mt-2">
+        <h2>Genres</h2>
+        <p v-for="genre in movie.genres">{{ genre.name }}</p>
+      </div>
+    </div>
+  </div>
+  <div class="box" v-else>
+    <h1>Something went wrong!</h1>
+    <h2>Try reloading the page.</h2>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ basic-frontend/src/components/ui/ReviewsBox.vue
@@ -0,0 +1,31 @@
+<script>
+import Review from "@/components/ui/Review.vue";
+
+export default {
+  name: "ReviewsBox",
+  props: {
+    ratings: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {Review}
+}
+</script>
+
+<template>
+  <div class="box" v-if="ratings.length">
+    <h1 class="text-center">Reviews</h1>
+    <div v-for="review in ratings">
+      <Review :id="review.id" :title="review.title" :content="review.content" :has_spoilers="review.has_spoilers"
+              :stars="review.stars" :uid="review.rater.id" :uname="review.rater.name"/>
+    </div>
+  </div>
+  <div class="box" v-else>
+    <h1 class="text-center">No reviews!</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
