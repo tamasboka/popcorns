02d35e78e2e580c9a8d090b42a7dbb55ca3b5a88
Bóka Tamás <tomi500offical@gmail.com>
2026-03-24 23:32:38 +0100

    feat: add title + search bar

--- basic-frontend/src/views/Public/HomeView.vue
+++ basic-frontend/src/views/Public/HomeView.vue
@@ -1,11 +1,51 @@
 <script>
+import {getAllMovies} from "@/data/data.js";
+
 export default {
-  name: "HomeView"
+  name: "HomeView",
+  data() {
+    return {
+      top: [],
+      newest: [],
+      filteredMovies: [],
+      searchQuery: ''
+    }
+  },
+  methods: {
+    getTopMovies() {
+
+    },
+    async getMovies() {
+      try {
+        const res = await getAllMovies()
+        this.movies = res.data.data
+      } catch {
+        this.error = true
+      }
+    },
+    filterMovies() {
+      if (this.searchQuery) this.filteredMovies = this.movies.filter(movie => movie.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+      else this.filteredMovies = []
+    }
+  },
+  mounted() {
+    this.getMovies()
+  }
 }
 </script>
 
 <template>
-
+  <section class="d-flex justify-content-center min-vh-100">
+    <div class="text-center my-5">
+      <h1>Popcorns!</h1>
+      <input v-model="searchQuery" @input="filterMovies" type="text" class="form-control" placeholder="Search for a movie">
+      <ul class="list-group" v-if="filteredMovies">
+        <li class="list-group-item" v-for="movie in filteredMovies">
+          <RouterLink :to="{name: 'public-movie', params: {movieID: movie.id}}" class="text-dark">{{ movie.title }}</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <style scoped>
