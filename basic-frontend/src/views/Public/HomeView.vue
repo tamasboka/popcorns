ab6d33dc470c16a5c0f708153ffd74a16f99639b
Bóka Tamás <tomi500offical@gmail.com>
2026-03-25 17:28:52 +0100

    remove redundant code

--- basic-frontend/src/views/Public/HomeView.vue
+++ basic-frontend/src/views/Public/HomeView.vue
@@ -1,43 +1,16 @@
 <script>
-import {getAllMovies} from "@/data/data.js";
 import MovieSearchbar from "@/components/ui/MovieSearchbar.vue";
 
 export default {
   name: "HomeView",
   components: {MovieSearchbar},
-  data() {
-    return {
-      top: [],
-      newest: [],
-    }
-  },
-  methods: {
-    getTopMovies() {
-
-    },
-    async getMovies() {
-      try {
-        const res = await getAllMovies()
-        this.movies = res.data.data
-      } catch {
-        this.error = true
-      }
-    },
-    filterMovies() {
-      if (this.searchQuery) this.filteredMovies = this.movies.filter(movie => movie.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
-      else this.filteredMovies = []
-    }
-  },
-  mounted() {
-    this.getMovies()
-  }
 }
 </script>
 
 <template>
   <section class="d-flex justify-content-center min-vh-100">
     <div class="text-center my-5">
-      <h1>Popcorns!</h1>
+      <h1>Popcorns!🍿</h1>
       <MovieSearchbar/>
     </div>
   </section>
