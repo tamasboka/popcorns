88bac93d7b38e5900f88325d455f4c034eca612b
Bóka Tamás <tomi500offical@gmail.com>
2026-03-27 15:40:27 +0100

    feat: create Series details view and details component

---
+++ basic-frontend/src/components/ui/SeriesDetails.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  name: "SeriesDetails",
+  props: {
+    series: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    releaseDate() {
+      let date = this.series.release_year
+      if (this.series.release_month && this.series.release_day) date += ". " + this.series.release_month + ". " + this.series.release_day + "."
+      return date
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="box" v-if="series">
+    <div class="row text-center">
+      <div class="col-12">
+        <h1 class="text-center">{{ series.title }}</h1>
+      </div>
+      <div class="col-12 col-lg-3 mt-2">
+        <h2>Rating</h2>
+        <h2 v-if="series.avg">⭐{{ series.avg }} / 10</h2>
+        <h2 v-else>No ratings</h2>
+      </div>
+      <div class="col-12 col-lg-3 mt-2">
+        <h2>Release date</h2>
+        <h2>{{ releaseDate }}</h2>
+      </div>
+      <div class="col-12 col-lg-3 mt-2">
+        <h2>Seasons</h2>
+        <h2>{{ series.count }}</h2>
+      </div>
+      <div class="col-12 col-lg-3 mt-2">
+        <h2>Genres</h2>
+        <p v-for="genre in series.genres">{{ genre.name }}</p>
+      </div>
+    </div>
+  </div>
+  <div class="box" v-else>
+    <h1>Something went wrong!</h1>
+    <h2>Try reloading the page.</h2>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- basic-frontend/src/views/Public/ShowView.vue
+++ basic-frontend/src/views/Public/ShowView.vue
@@ -1,6 +1,9 @@
 <script>
+import SeriesDetails from "@/components/ui/SeriesDetails.vue";
+
 export default {
   name: "ShowView",
+  components: {SeriesDetails},
   data() {
     return {
       series: this.$route.meta.prefetched.show.data.data
@@ -10,7 +13,11 @@ export default {
 </script>
 
 <template>
-
+  <section class="row p-5">
+    <div class="col-12 col-xl-12 col-lg-12 col-md-12 col-sm-12">
+      <SeriesDetails :series="series"/>
+    </div>
+  </section>
 </template>
 
 <style scoped>
