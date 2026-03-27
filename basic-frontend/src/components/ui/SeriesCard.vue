30c6fdc67a33e3c99f3285f8c48356385341f74b
Bóka Tamás <tomi500offical@gmail.com>
2026-03-27 19:20:44 +0100

    feat: series card color is based on rating

--- basic-frontend/src/components/ui/SeriesCard.vue
+++ basic-frontend/src/components/ui/SeriesCard.vue
@@ -6,12 +6,22 @@ export default {
       type: Object,
       required: true
     }
+  },
+  computed: {
+    ratingColor() {
+      if (!this.series.avg) return 'unrated'
+      if (this.series.avg <= 2) return 'very-bad'
+      if (this.series.avg <= 4) return 'bad'
+      if (this.series.avg <= 6) return 'mid'
+      if (this.series.avg <= 8) return 'good'
+      if (this.series.avg <= 10) return 'very-good'
+    }
   }
 }
 </script>
 
 <template>
-  <div class="card m-2">
+  <div class="card m-2" :class="ratingColor">
     <div class="card-body text-center" @click="$router.push({name: 'public-show', params: {showID: series.id}})">
       <p class="card-text">{{ series.title }}</p>
       <p class="card-text">{{ series.avg? movie.avg+'⭐' : 'No ratings yet' }}</p>
