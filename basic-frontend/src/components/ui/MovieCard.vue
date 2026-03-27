d23b888698b1383a9f8d4953379ab2e4408cf02d
Bóka Tamás <tomi500offical@gmail.com>
2026-03-27 19:18:06 +0100

    feat: movie card color is based on rating

--- basic-frontend/src/components/ui/MovieCard.vue
+++ basic-frontend/src/components/ui/MovieCard.vue
@@ -6,12 +6,22 @@ export default {
       type: Object,
       required: true
     }
+  },
+  computed: {
+    ratingColor() {
+      if (!this.movie.avg) return 'unrated'
+      if (this.movie.avg <= 2) return 'very-bad'
+      if (this.movie.avg <= 4) return 'bad'
+      if (this.movie.avg <= 6) return 'mid'
+      if (this.movie.avg <= 8) return 'good'
+      if (this.movie.avg <= 10) return 'very-good'
+    }
   }
 }
 </script>
 
 <template>
-  <div class="card m-2">
+  <div class="card m-2" :class="ratingColor">
     <div class="card-body text-center" @click="$router.push({name: 'public-movie', params: {movieID: movie.id}})">
       <p class="card-text">{{ movie.title }}</p>
       <p class="card-text">{{ movie.avg? movie.avg+'⭐' : 'No ratings yet' }}</p>
