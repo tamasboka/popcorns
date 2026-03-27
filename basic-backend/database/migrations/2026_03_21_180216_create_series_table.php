44c1eae081e5d94a6d2b60b364f2d83809f31c36
Bóka Tamás <tomi500offical@gmail.com>
2026-03-27 15:18:39 +0100

    feat: make actor, director, writer, watched, genre pivot tables & release year, month, day columns for series table

--- basic-backend/database/migrations/2026_03_21_180216_create_series_table.php
+++ basic-backend/database/migrations/2026_03_21_180216_create_series_table.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Models\Genre;
+use App\Models\Person;
+use App\Models\Series;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +17,70 @@ return new class extends Migration
         Schema::create('series', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->integer('release_year');
+            $table->integer('release_month');
+            $table->integer('release_day');
+            $table->timestamps();
+        });
+
+        Schema::create('actor_series', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Series::class)
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreignIdFor(Person::class, 'actor_id')
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+
+        Schema::create('director_series', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Series::class)
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreignIdFor(Person::class, 'director_id')
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+
+        Schema::create('series_writer', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Series::class)
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreignIdFor(Person::class, 'writer_id')
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+
+        Schema::create('series_watched', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Series::class)
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+
+        Schema::create('genre_series', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Genre::class)
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignIdFor(Series::class)
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
