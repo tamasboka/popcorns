a4df0904b997ed714f455a406d5e7186b7dec1a7
Bóka Tamás <tomi500offical@gmail.com>
2026-04-13 20:41:01 +0200

    feat: create basic welcome message for admin home

--- frontend/src/views/Admin/AdminHome.vue
+++ frontend/src/views/Admin/AdminHome.vue
@@ -1,11 +1,21 @@
 <script>
 export default {
-  name: "AdminHome"
+  name: "AdminHome",
+  data() {
+    return {
+      username: localStorage.getItem('popcorns_name')
+    }
+  }
 }
 </script>
 
 <template>
-
+<section class="d-flex justify-content-center align-items-center min-vh-100">
+  <div class="p-5 box d-block">
+    <h1>Welcome back, {{ username }}!</h1>
+    <p>Remember: Power tripping is fun, but punishable.</p>
+  </div>
+</section>
 </template>
 
 <style scoped>
