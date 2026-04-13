80328de33ae65d93d34369dbdb7c480195825407
Bóka Tamás <tomi500offical@gmail.com>
2026-04-13 20:49:17 +0200

    finish remaining functions + add authorization

--- backend/app/Http/Controllers/Api/GenreController.php
+++ backend/app/Http/Controllers/Api/GenreController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\Genre\UpdateGenreRequest;
 use App\Http\Resources\Genre\GenreCollection;
 use App\Http\Resources\Genre\GenreResource;
 use App\Models\Genre;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
@@ -16,12 +17,17 @@ class GenreController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $genres = Genre::all();
-        return (new GenreCollection($genres))
-            ->response()
-            ->setStatusCode(200);
+        if ($request->user()->tokenCan('admin')) {
+            $genres = Genre::all();
+            return (new GenreCollection($genres))
+                ->response()
+                ->setStatusCode(200);
+        }
+        return response()->json([
+            "message" => "Unauthorized"
+        ], 401);
     }
 
     /**
@@ -29,46 +35,85 @@ class GenreController extends Controller
      */
     public function store(StoreGenreRequest $request)
     {
-        try {
-            $genre = Genre::create($request->all());
-            return (new GenreResource($genre))->response()->setStatusCode(201);
-        } catch (ValidationException $e) {
-            return response()->json([
-                'success' => false,
-                'error' => $e->getMessage()
-            ], 422);
+        if ($request->user()->tokenCan('admin')) {
+            try {
+                $genre = Genre::create($request->all());
+                return (new GenreResource($genre))->response()->setStatusCode(201);
+            } catch (ValidationException $e) {
+                return response()->json([
+                    'success' => false,
+                    'error' => $e->getMessage()
+                ], 422);
+            }
         }
+        return response()->json([
+            "message" => "Unauthorized"
+        ], 401);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Genre $genre)
+    public function show(string $id, Request $request)
     {
-        //
+        if ($request->user()->tokenCan('admin')) {
+            try {
+                $genre = Genre::with('movies')->findOrFail($id);
+            } catch (ModelNotFoundException $e) {
+                return response()->json([
+                    "message" => "Genre not found"
+                ], 404);
+            }
+            return (new GenreResource($genre))
+                ->response()
+                ->setStatusCode(200);
+        }
+        return response()->json([
+            "message" => "Unauthorized"
+        ], 401);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateGenreRequest $request, Genre $genre)
+    public function update(UpdateGenreRequest $request, string $id)
     {
-        //
+        if ($request->user()->tokenCan('admin')) {
+            try {
+                $genre = Genre::findOrFail($id);
+            } catch (ModelNotFoundException $e) {
+                return response()->json([
+                    "message" => "Genre not found"
+                ], 404);
+            }
+            $genre->update($request->all());
+            return (new GenreResource($genre))
+                ->response()
+                ->setStatusCode(200);
+        }
+        return response()->json([
+            "message" => "Unauthorized"
+        ], 401);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Genre $genre)
+    public function destroy(Genre $genre, Request $request)
     {
-        try {
-            $genre->delete();
-            return response()->json([], 204);
-        } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'error' => $e->getMessage()
-            ], 204);
+        if ($request->user()->tokenCan('admin')) {
+            try {
+                $genre->delete();
+                return response()->json([], 204);
+            } catch (\Exception $e) {
+                return response()->json([
+                    'success' => false,
+                    'error' => $e->getMessage()
+                ], 204);
+            }
         }
+        return response()->json([
+            "message" => "Unauthorized"
+        ], 401);
     }
 }
